89088c1e775ffdcf404ccb2662b813f479b4f964
dhnyekaputra <pramesta.dhany@gmail.com>
2025-01-03 14:50:16 +0700

    Refactor the Calculation Program

--- main.go
+++ main.go
@@ -2,48 +2,6 @@ package main
 
 import "fmt"
 
-func calcSquares(number int, squareop chan int) {
-	sum := 0
-	for number != 0 {
-		digit := number % 10
-		sum += digit * digit
-		number /= 10
-	}
-
-	fmt.Println("Square Calculation Done!")
-	squareop <- sum
-
-}
-
-func calcCubes(number int, cubeop chan int) {
-	sum := 0
-
-	for number != 0 {
-		digit := number % 10
-		sum += digit * digit * digit
-		number /= 10
-	}
-
-	fmt.Println("Cube Calculation Done!")
-	cubeop <- sum
-
-}
-
-func calcTotal() int {
-	number := 982378273819287399
-	sqrch := make(chan int)
-	cubech := make(chan int)
-
-	go calcSquares(number, sqrch)
-	go calcCubes(number, cubech)
-
-	squares, cubes := <-sqrch, <-cubech
-	total := squares + cubes
-
-	fmt.Println("Total Calculation Done!")
-	return total
-}
-
 func main() {
 
 	fmt.Println("Final output", calcTotal())
---
+++ squarecubes.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+// function takes int as first parameter
+// the second param is channel, we'll use that later for storing our calc value
+func calcSquares(number int, squareop chan int) {
+	sum := 0
+	for number != 0 {
+		digit := number % 10
+		sum += digit * digit
+		number /= 10
+	}
+
+	fmt.Println("Square Calculation Done!")
+	squareop <- sum
+
+}
+
+// function takes int as first parameter
+// the second param is channel, we'll use that later for storing our calc value
+func calcCubes(number int, cubeop chan int) {
+	sum := 0
+
+	for number != 0 {
+		digit := number % 10
+		sum += digit * digit * digit
+		number /= 10
+	}
+
+	fmt.Println("Cube Calculation Done!")
+	cubeop <- sum
+
+}
+
+func calcTotal() int {
+	number := 982378273819287399
+	sqrch := make(chan int)
+	cubech := make(chan int)
+
+	// calcSquares goroutine will start
+	go calcSquares(number, sqrch)
+
+	// calcCubes goroutine will start
+	go calcCubes(number, cubech)
+
+	// program will stop until sqrch and cubech
+	// receiving data from the function
+	squares, cubes := <-sqrch, <-cubech
+	total := squares + cubes
+
+	fmt.Println("Total Calculation Done!")
+	return total
+}
